3f05e87a2e3c2e8798033fc30ad64976a883541b
Mohammed Zeeshan <mohammedzeeshan9577@gmail.com>
2026-07-15 22:51:09 +0530

    Time: 0 ms (100%), Space: 43.4 MB (6.49%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,20 +1,13 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        int i = s.length() - 1;
+        int i = s.length() - 1 ;
 
-        // Skip trailing spaces
-        while (i >= 0 && s.charAt(i) == ' ') {
-            i--;
+        while(i >= 0 && s.charAt(i) == ' ' ) i-- ;
+        int length = 0 ;
+        while(i >= 0 && s.charAt(i) != ' ' ) {
+            length++ ;
+            i-- ;
         }
-
-        int length = 0;
-
-        // Count characters of the last word
-        while (i >= 0 && s.charAt(i) != ' ') {
-            length++;
-            i--;
-        }
-
-        return length;
+        return length ;
     }
 }

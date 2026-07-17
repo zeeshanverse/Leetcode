e22fea74e4d9c71f45a1606a26f7a031e2551fbb
Mohammed Zeeshan <mohammedzeeshan9577@gmail.com>
2026-07-17 23:18:42 +0530

    Time: 33 ms (39.74%), Space: 57.6 MB (36.47%) - LeetHub

---
+++ 0706-design-hashmap/0706-design-hashmap.java
@@ -0,0 +1,23 @@
+import java.util.Arrays;
+
+class MyHashMap {
+
+    private int[] map;
+
+    public MyHashMap() {
+        map = new int[1000001];
+        Arrays.fill(map, -1);
+    }
+
+    public void put(int key, int value) {
+        map[key] = value;
+    }
+
+    public int get(int key) {
+        return map[key];
+    }
+
+    public void remove(int key) {
+        map[key] = -1;
+    }
+}

95feb88ff7d53ca08723575b7468a0fd56190a23
Mohammed Zeeshan <mohammedzeeshan9577@gmail.com>
2026-07-26 13:20:42 +0530

    Time: 1 ms (97.2%), Space: 46.9 MB (96.15%) - LeetHub

--- 0203-remove-linked-list-elements/0203-remove-linked-list-elements.java
+++ 0203-remove-linked-list-elements/0203-remove-linked-list-elements.java
@@ -8,48 +8,44 @@
  *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
  * }
  */
-// class Solution {
-//     public ListNode removeElements(ListNode head, int val) {
-//         if(head == null ) return head ;
-//         if(head.val == val ) {
-//             ListNode temp = head ;
-//             head = head.next ;
-//             return head ;
-//         }
-//         ListNode temp = head ;
-//         ListNode prev = null ;
-//         while(temp != null ) {
-//             // prev = temp ;
-//             if(temp.val == val ) {
-//                 prev.next = prev.next.next ;
-//                 break ;
-//             }
-//             prev = temp ;
-//             temp = temp.next ;
-//         }
-//         return head ;
-//     }
-// }
 class Solution {
     public ListNode removeElements(ListNode head, int val) {
+        
+        ListNode dummy = new ListNode(0) ;
+        dummy.next = head ;
 
-        ListNode dummy = new ListNode(0);
-        dummy.next = head;
+        ListNode prev = dummy ;
+        ListNode temp = head ;
 
-        ListNode prev = dummy;
-        ListNode curr = head;
-
-        while (curr != null) {
-
-            if (curr.val == val) {
-                prev.next = curr.next;
-            } else {
-                prev = curr;
-            }
-
-            curr = curr.next;
+        while(temp != null ) {
+            if(temp.val == val ) {
+                prev.next = temp.next ;
+            }else prev = temp ;
+            temp = temp.next ;
         }
-
-        return dummy.next;
+        return dummy.next ;
     }
 }
+// class Solution {
+//     public ListNode removeElements(ListNode head, int val) {
+
+//         ListNode dummy = new ListNode(0);
+//         dummy.next = head;
+
+//         ListNode prev = dummy;
+//         ListNode curr = head;
+
+//         while (curr != null) {
+
+//             if (curr.val == val) {
+//                 prev.next = curr.next;
+//             } else {
+//                 prev = curr;
+//             }
+
+//             curr = curr.next;
+//         }
+
+//         return dummy.next;
+//     }
+// }

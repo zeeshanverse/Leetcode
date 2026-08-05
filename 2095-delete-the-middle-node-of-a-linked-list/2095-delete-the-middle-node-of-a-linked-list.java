8cf6f153e73255a703db16c456944363bdd41aea
Mohammed Zeeshan <mohammedzeeshan9577@gmail.com>
2026-08-05 23:30:31 +0530

    Time: 3 ms (99.95%), Space: 202.4 MB (87.08%) - LeetHub

--- 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.java
+++ 2095-delete-the-middle-node-of-a-linked-list/2095-delete-the-middle-node-of-a-linked-list.java
@@ -11,26 +11,41 @@
 class Solution {
     public ListNode deleteMiddle(ListNode head) {
 
+        // if(head == null || head.next == null ) return null ;
+
+        // ListNode temp = head ;
+        // int n = 0 ;
+        
+        // while(temp != null ) {
+        //     n++ ;
+        //     temp = temp.next ;
+        // }
+        // temp = head ;
+        // int res = n / 2 ;
+        // while(temp != null ) {
+        //     res-- ;
+        //     if(res == 0 ) {
+        //         ListNode middle = temp.next ;
+        //         temp.next = temp.next.next ;
+        //         break ;
+        //     }
+        //     temp = temp.next ;
+        // }
+        // return head ;
+
         if(head == null || head.next == null ) return null ;
 
-        ListNode temp = head ;
-        int n = 0 ;
+        ListNode slow = head ;
+        ListNode fast = head ;
+
+        fast = fast.next.next ;
+        while(fast != null && fast.next != null ) {
+            slow = slow.next ;
+            fast = fast.next.next ;
+        }
+        slow.next = slow.next.next ;
         
-        while(temp != null ) {
-            n++ ;
-            temp = temp.next ;
-        }
-        temp = head ;
-        int res = n / 2 ;
-        while(temp != null ) {
-            res-- ;
-            if(res == 0 ) {
-                ListNode middle = temp.next ;
-                temp.next = temp.next.next ;
-                break ;
-            }
-            temp = temp.next ;
-        }
         return head ;
+
     }
 }

0f4043d2232beb50cb0883018a5f9c4e9635fbe1
Mohammed Zeeshan <mohammedzeeshan9577@gmail.com>
2026-08-19 23:42:12 +0530

    Time: 0 ms (100%), Space: 44.2 MB (92.88%) - LeetHub

--- 0022-generate-parentheses/0022-generate-parentheses.java
+++ 0022-generate-parentheses/0022-generate-parentheses.java
@@ -3,37 +3,42 @@ class Solution {
     public List<String> generateParenthesis(int n) {
         StringBuilder curr = new StringBuilder("") ;
 
-        solve(curr , n ) ;
+        int open = 0 ;
+        int close = 0 ;
+
+        solve(curr , n , open , close ) ;
 
         return ans ;
     }
 
-    void solve(StringBuilder s , int n ) {
+    void solve(StringBuilder s , int n, int open , int close  ) {
         if(s.length() == 2 * n ) {
-            if(isValid(s)) ans.add(s.toString()) ;
-
+            ans.add(s.toString()) ;
             return ;
         }
 
-        s.append("(") ;
-        solve(s , n ) ;
-        s.deleteCharAt(s.length() - 1 ) ;
-
-        s.append(")") ;
-        solve(s , n ) ;
-        s.deleteCharAt(s.length() - 1 ) ;
-
-    }
-
-    boolean isValid(StringBuilder str ) {
-        int count = 0 ;
-
-        for(int i = 0 ; i < str.length() ; i++ ) {
-            if(str.charAt(i)== '(') count++ ;
-            else count-- ;
-
-            if(count < 0 ) return false ;
+        if(open < n ){
+            s.append("(") ;
+            solve(s , n , open + 1 , close ) ;
+            s.deleteCharAt(s.length() - 1 ) ;
         }
-        return count == 0 ;
+
+        if(close < open ) {
+            s.append(")") ;
+            solve(s , n , open , close + 1 ) ;
+            s.deleteCharAt(s.length() - 1 ) ;
+        }
     }
+
+    // boolean isValid(StringBuilder str ) {
+    //     int count = 0 ;
+
+    //     for(int i = 0 ; i < str.length() ; i++ ) {
+    //         if(str.charAt(i)== '(') count++ ;
+    //         else count-- ;
+
+    //         if(count < 0 ) return false ;
+    //     }
+    //     return count == 0 ;
+    // }
 }

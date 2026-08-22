4425bdc13d82f29a5072e48a8c7eca4bf1ce3f52
Mohammed Zeeshan <mohammedzeeshan9577@gmail.com>
2026-08-22 15:40:47 +0530

    Time: 33 ms (56.77%), Space: 62.6 MB (95.81%) - LeetHub

--- 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.java
+++ 1498-number-of-subsequences-that-satisfy-the-given-sum-condition/1498-number-of-subsequences-that-satisfy-the-given-sum-condition.java
@@ -1,37 +1,30 @@
 class Solution {
     public int numSubseq(int[] nums, int target) {
-        int MOD = 1000000007;
+        int mod = 1000000007 ;
+        long ans = 0 ;
 
-        Arrays.sort(nums);
+        Arrays.sort(nums) ;
 
-        int n = nums.length;
+        int n = nums.length ;
 
-        // power[i] = 2^i % MOD
-        long[] power = new long[n];
-        power[0] = 1;
-
-        for (int i = 1; i < n; i++) {
-            power[i] = (power[i - 1] * 2) % MOD;
+        long[] powers = new long[n] ;
+        powers[0] = 1 ;
+        for(int i = 1 ; i < n ; i++ ){
+            powers[i] = (powers[i - 1 ] * 2 ) % mod ;
         }
 
-        int left = 0;
-        int right = n - 1;
+        int left = 0 ;
+        int right = n - 1 ;
 
-        long ans = 0;
-
-        while (left <= right) {
-
-            if (nums[left] + nums[right] <= target) {
-
-                ans = (ans + power[right - left]) % MOD;
+        while(left <= right ) {
+            if(nums[left] + nums[right] <= target ) {
+                ans = (ans + powers[right - left ]) % mod ;
 
                 left++;
-
-            } else {
-                right--;
-            }
+            }else right-- ;
         }
 
-        return (int) ans;
+        return (int) ans ;
+
     }
 }

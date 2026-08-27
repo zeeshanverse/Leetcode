74d2caa4b7ea64713d9313437b68da0968a25f14
Mohammed Zeeshan <mohammedzeeshan9577@gmail.com>
2026-08-27 23:40:00 +0530

    Time: 128 ms (47.83%), Space: 52.8 MB (52.66%) - LeetHub

--- 0282-expression-add-operators/0282-expression-add-operators.java
+++ 0282-expression-add-operators/0282-expression-add-operators.java
@@ -1,12 +1,12 @@
 class Solution {
+    List<String> ans = new ArrayList<>() ;
     public List<String> addOperators(String num, int target) {
-        List<String> ans = new ArrayList<>() ;
-        
-        solve( 0 , "" , 0 , 0 , num , ans , target )  ;
+
+        solve( 0 , "" , 0 , 0 , num , target )  ;
 
         return ans ;
     }
-    void solve(int idx , String temp , long resSoFar , long prevNum , String str , List<String> ans , int      target) {
+    void solve(int idx , String temp , long resSoFar , long prevNum , String str , int target) {
         if(idx == str.length()) {
             if(resSoFar == target ) ans.add(temp) ;
 
@@ -16,11 +16,11 @@ class Solution {
         for(int i = idx  ; i < str.length() ; i++ ) {
             if(i > idx && str.charAt(idx) == '0' ) break ;
             long currNum = Long.parseLong(str.substring(idx , i + 1)) ;
-            if(idx == 0 ) solve(i + 1 , temp + currNum , currNum , currNum , str , ans , target) ;
+            if(idx == 0 ) solve(i + 1 , temp + currNum , currNum , currNum , str , target) ;
             else{
-                solve(i + 1 , temp + "+" + currNum , resSoFar + currNum , currNum , str , ans , target ) ;
-                solve(i + 1 , temp + "-" + currNum , resSoFar - currNum , -currNum , str , ans , target ) ;
-                solve(i + 1 , temp + "*" + currNum , resSoFar - prevNum + prevNum * currNum , prevNum * currNum , str , ans , target ) ;
+                solve(i + 1 , temp + "+" + currNum , resSoFar + currNum , currNum , str , target ) ;
+                solve(i + 1 , temp + "-" + currNum , resSoFar - currNum , -currNum , str , target ) ;
+                solve(i + 1 , temp + "*" + currNum , resSoFar - prevNum + prevNum * currNum , prevNum * currNum , str , target ) ;
             }
         }
 

ecbc1d6b8e339f3367c43e0ec9e18cc16a92bb05
Mohammed Zeeshan <mohammedzeeshan9577@gmail.com>
2026-08-29 21:22:20 +0530

    Time: 2 ms (97.05%), Space: 45.3 MB (12.5%) - LeetHub

--- 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
+++ 0349-intersection-of-two-arrays/0349-intersection-of-two-arrays.java
@@ -4,14 +4,13 @@ class Solution {
 
         int [] ans = new int[Math.min(nums1.length , nums2.length)] ;
 
-        for(int x : nums1 ) set.add(x) ;
-
         int k = 0 ;
+        
+        for(int x : nums1 ) set.add(x) ;
 
         for(int i = 0 ; i < nums2.length ; i++ ) {
             if(set.contains(nums2[i])) {
-                ans[k] = nums2[i] ;
-                k++ ;
+                ans[k++] = nums2[i] ;
                 set.remove(nums2[i]) ;
             }
         }

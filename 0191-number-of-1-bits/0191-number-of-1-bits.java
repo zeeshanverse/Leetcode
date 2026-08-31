95e43b47d15639d84d31b06384d0a93a334ffed8
Mohammed Zeeshan <mohammedzeeshan9577@gmail.com>
2026-08-31 21:27:32 +0530

    Time: 0 ms (100%), Space: 42.5 MB (47.03%) - LeetHub

--- 0191-number-of-1-bits/0191-number-of-1-bits.java
+++ 0191-number-of-1-bits/0191-number-of-1-bits.java
@@ -1,22 +1,33 @@
-class Solution {
-    public int hammingWeight(int n) {
-        StringBuilder res = new StringBuilder("") ;
+// class Solution {
+//     public int hammingWeight(int n) {
+//         StringBuilder res = new StringBuilder("") ;
         
-        if (n == 0) return 0 ;
+//         if (n == 0) return 0 ;
 
-        while(n != 0 ) {
-            if(n % 2 == 1 ) res.append('1') ;
-            else res.append('0') ;
+//         while(n != 0 ) {
+//             if(n % 2 == 1 ) res.append('1') ;
+//             else res.append('0') ;
             
-            n = n / 2 ;
-        }
-        String temp = res.reverse().toString() ;
+//             n = n / 2 ;
+//         }
+//         String temp = res.reverse().toString() ;
 
-        int count = 0 ;
+//         int count = 0 ;
         
-        for(int i = 0 ; i < temp.length() ; i++ ) {
-            if(temp.charAt(i) == '1') count++ ;
+//         for(int i = 0 ; i < temp.length() ; i++ ) {
+//             if(temp.charAt(i) == '1') count++ ;
+//         }
+//         return count ;
+//     }
+// }
+
+public class Solution {
+    public int hammingWeight(int n) {
+        int count = 0;
+        while (n != 0) {
+            n &= (n - 1); // Clears the lowest set bit
+            count++;
         }
-        return count ;
+        return count;
     }
 }

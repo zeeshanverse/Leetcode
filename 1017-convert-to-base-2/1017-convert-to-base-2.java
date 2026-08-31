6f243100f54bc4bf762c6781afdeef99977e5b4c
Mohammed Zeeshan <mohammedzeeshan9577@gmail.com>
2026-08-31 21:21:52 +0530

    Time: 1 ms (99.08%), Space: 42.7 MB (44.62%) - LeetHub

---
+++ 1017-convert-to-base-2/1017-convert-to-base-2.java
@@ -0,0 +1,18 @@
+class Solution {
+    public String baseNeg2(int n) {
+        if(n == 0 ) return "0" ;
+
+        StringBuilder res = new StringBuilder("") ;
+        
+        while( n != 0 ) {
+            int rem = n % -2 ;
+
+            if(rem < 0 ) rem += 2 ;
+
+            res.append(rem) ;
+
+            n = (n - rem ) / -2 ;
+        }
+        return res.reverse().toString() ;
+    }
+}

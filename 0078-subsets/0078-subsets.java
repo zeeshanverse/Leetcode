6d5ba0c12d68320b923f23e22bc6585944acb3f4
Mohammed Zeeshan <mohammedzeeshan9577@gmail.com>
2026-09-03 23:55:15 +0530

    Time: 1 ms (86.79%), Space: 44.4 MB (6.79%) - LeetHub

--- 0078-subsets/0078-subsets.java
+++ 0078-subsets/0078-subsets.java
@@ -1,16 +1,15 @@
 class Solution {
     public List<List<Integer>> subsets(int[] nums) {
         List<List<Integer>> ans = new ArrayList<>() ;
+
+        int subsets = 1 << nums.length ;
         
-        int n = nums.length ;
+        for(int i = 0 ; i < subsets ; i++ ) {
+            List<Integer> temp = new ArrayList<>() ;
 
-        for(int i = 0 ; i < 1 << n ; i++ ) {
-            List<Integer> list = new ArrayList<>() ;
+            for(int j = 0 ; j < nums.length ; j++ ) if((i & (1 << j)) != 0 ) temp.add(nums[j]) ;
 
-            for(int j = 0 ; j < n ; j++ ) {
-                if((i & (1 << j )) != 0 ) list.add(nums[j]) ;
-            }
-            ans.add(list) ;
+            ans.add(temp) ;
         }
         return ans ;
     }

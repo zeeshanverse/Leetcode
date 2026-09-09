7e94873e1dff595736f0ab54b58d83cbfa28d47a
Mohammed Zeeshan <mohammedzeeshan9577@gmail.com>
2026-09-09 23:24:58 +0530

    Time: 2 ms (72.68%), Space: 43.8 MB (79.6%) - LeetHub

---
+++ 3174-clear-digits/3174-clear-digits.java
@@ -0,0 +1,12 @@
+class Solution {
+    public String clearDigits(String s) {
+        StringBuilder ans = new StringBuilder();
+
+        for (char ch : s.toCharArray()) {
+            if (Character.isDigit(ch)) ans.deleteCharAt(ans.length() - 1);
+            else ans.append(ch);
+        }
+
+        return ans.toString();
+    }
+}
